f86df19c1edc32b3167ca0495401e40e1da71276
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2019-05-20 14:05:23 +1000

    x

--- UbsRec/yahoo/yahoo.py
+++ UbsRec/yahoo/yahoo.py
@@ -1,19 +1,22 @@
 from os import path
 
 import numpy as np
+import os
 import pandas as pd
 
 def main():
-  data_dir = path.expanduser('~/Downloads/data/Webscope_R3')
-  if not path.exists(data_dir):
+  in_dir = path.expanduser('~/Downloads/data/Webscope_R3')
+  if not path.exists(in_dir):
     raise Exception('Please download the yahoo dataset.')
 
-  biased_file = path.join(data_dir, 'ydata-ymusic-rating-study-v1_0-train.txt')
-  unbiased_file = path.join(data_dir, 'ydata-ymusic-rating-study-v1_0-test.txt')
+  biased_file = path.join(in_dir, 'ydata-ymusic-rating-study-v1_0-train.txt')
+  unbiased_file = path.join(in_dir, 'ydata-ymusic-rating-study-v1_0-test.txt')
   read_kwargs = {'sep': '\t', 'names': ['user', 'item', 'rating']}
   biased_ratings = pd.read_csv(biased_file, **read_kwargs)
   unbiased_ratings = pd.read_csv(unbiased_file, **read_kwargs)
+  print('original: ', unbiased_ratings[:3])
   unbiased_ratings = unbiased_ratings.sample(frac=1)
+  print('shuffled: ', unbiased_ratings[:3])
 
   n_user = biased_ratings.user.unique().shape[0]
   n_item = biased_ratings.item.unique().shape[0]
@@ -24,9 +27,12 @@ def main():
   print('#biased=%d #unbiased=%d' % (n_biased, n_unbiased))
 
   valid_ratio = 0.1
-  train_file = path.join(data_dir, 'train.dta')
-  valid_file = path.join(data_dir, 'valid.dta')
-  test_file = path.join(data_dir, 'test.dta')
+  out_dir = 'data'
+  if not path.exists(out_dir):
+    os.makedirs(out_dir)
+  train_file = path.join(out_dir, 'train.dta')
+  valid_file = path.join(out_dir, 'valid.dta')
+  test_file = path.join(out_dir, 'test.dta')
   to_kwargs = {'sep': '\t', 'header': False, 'index': False, 
                'columns': ['rating', 'user', 'item']}
   train_ratings = biased_ratings
