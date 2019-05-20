6e090472b102ca579ff3fd664a79e11d9f75d619
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2019-05-20 16:26:20 +1000

    x

--- UbsRec/coat/coat.py
+++ UbsRec/coat/coat.py
@@ -9,82 +9,64 @@ class Dataset(object):
     self.arg = arg
 
 def maybe_download(in_dir):
-  coat_url = 'https://www.cs.cornell.edu/~schnabts/mnar/coat.zip'
-
   if path.exists(in_dir):
     return
-
   in_dir = path.dirname(in_dir)
   if not path.exists(in_dir):
     os.makedirs(in_dir)
-
+  coat_url = 'https://www.cs.cornell.edu/~schnabts/mnar/coat.zip'
   coat_zip = path.join(in_dir, 'data.zip')
   os.system('wget %s -O %s' % (coat_url, coat_zip))
   os.system('unzip %s -d %s' % (coat_zip, in_dir))
   os.system('rm -f %s' % (coat_zip))
 
 def load_dataset(in_file):
-  dns_ratings = np.loadtxt(in_file, dtype=np.int32)
-  n_user, n_item = dns_ratings.shape
-
-  coo_ratings = []
-  users, items = dns_ratings.nonzero()
+  dense_data = np.loadtxt(in_file, dtype=np.int32)
+  n_user, n_item = dense_data.shape
+  coo_data = []
+  users, items = dense_data.nonzero()
   for user, item in zip(users, items):
-    rating = dns_ratings[user, item]
-    item += n_user # crucial
-    coo_ratings.append((user, item, rating))
-  return n_user, n_item, coo_ratings
+    rating = dense_data[user, item]
+    coo_data.append((user, item, rating))
+  return n_user, n_item, coo_data
 
 def save_dataset(ratings, out_file):
   n_rating = len(ratings)
   with open(out_file, 'w') as fout:
     for user, item, rating in ratings:
-      fout.write('%d\t%d\t%d\n' % (rating, user, item))
+      fout.write('%d\t%d\t%d\n' % (user, item, rating))
   print('save %d ratings to %s' % (n_rating, out_file))
 
-def split_dataset(in_dir, out_dir):
+def shuffle_data(in_dir, data_dir):
+  if path.exists(data_dir):
+    return
   biased_file = path.join(in_dir, 'train.ascii')
   unbiased_file = path.join(in_dir, 'test.ascii')
-  n_user, n_item, biased_ratings = load_dataset(biased_file)
-  n_user, n_item, unbiased_ratings = load_dataset(unbiased_file)
+  n_user, n_item, biased_data = load_dataset(biased_file)
+  n_user, n_item, unbiased_data = load_dataset(unbiased_file)
   print('#user=%d #item=%d' % (n_user, n_item))
-
-  n_biased = len(biased_ratings)
-  n_unbiased = len(unbiased_ratings)
+  n_biased = len(biased_data)
+  n_unbiased = len(unbiased_data)
   print('#biased=%d #unbiased=%d' % (n_biased, n_unbiased))
-
-  valid_ratio = 0.1
-  train_ratings = biased_ratings
   np.random.seed(0)
-  np.random.shuffle(unbiased_ratings)
-  n_valid = int(valid_ratio * n_unbiased)
-  valid_ratings = unbiased_ratings[:n_valid]
-  test_ratings = unbiased_ratings[n_valid:]
-
-  n_train = len(train_ratings)
-  n_valid = len(valid_ratings)
-  n_test = len(test_ratings)
-  print('#train=%d #valid=%d #test=%d' % (n_train, n_valid, n_test))
-
-  if not path.exists(out_dir):
-    os.makedirs(out_dir)
-  train_file = path.join(out_dir, 'train.dta')
-  valid_file = path.join(out_dir, 'valid.dta')
-  test_file = path.join(out_dir, 'test.dta')
-  save_dataset(train_ratings, train_file)
-  save_dataset(valid_ratings, valid_file)
-  save_dataset(test_ratings, test_file)
+  np.random.shuffle(biased_data)
+  np.random.shuffle(unbiased_data)
+  os.makedirs(data_dir)
+  biased_file = path.join(data_dir, 'biased.dta')
+  unbiased_file = path.join(data_dir, 'unbiased.dta')
+  save_dataset(biased_data, biased_file)
+  save_dataset(unbiased_data, unbiased_file)
 
 def main():
-  parser = argparse.ArgumentParser()
-  parser.add_argument('out_format', type=str)
-  parser.add_argument('ubs_ratio', type=float)
-  args = parser.parse_args()
+  # parser = argparse.ArgumentParser()
+  # parser.add_argument('out_format', type=str)
+  # parser.add_argument('ubs_ratio', type=float)
+  # args = parser.parse_args()
 
   in_dir = path.expanduser('~/Downloads/data/coat')
   maybe_download(in_dir)
-  out_dir = 'data'
-  split_dataset(in_dir, out_dir)
+  data_dir = 'data'
+  shuffle_data(in_dir, data_dir)
 
 if __name__ == '__main__':
   main()
