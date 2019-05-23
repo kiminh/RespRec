9fcddf13e79086165bbd46cd201292b308716632
xiaojiew1 <xiaojiew1@student.unimelb.edu.au>
2019-05-23 21:50:57 +1000

    x

--- UbsRec/coat/run.sh
+++ UbsRec/coat/run.sh
@@ -1,6 +1,6 @@
 for ubs_ratio in 0.01 0.05 0.1 0.5; do
-  python coat.py lib ${ubs_ratio}
-  # python coat.py coat ${ubs_ratio}
+  # python coat.py lib ${ubs_ratio}
+  python coat.py coat ${ubs_ratio}
 done
 exit
 
