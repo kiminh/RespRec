82ba2da9d3fc9da326e54bdb3e83f5f61f2943b3
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2019-06-17 14:22:59 +1000

    x

---
+++ UbsRec/nfm/run_all.sh
@@ -0,0 +1,15 @@
+python NeuralFM.py \
+  --path ~/Downloads/data/ \
+  --dataset frappe \
+  --hidden_factor 64 \
+  --layers '[64]' \
+  --keep_prob '[0.8,0.5]' \
+  --loss_type square_loss \
+  --activation relu \
+  --pretrain 0 \
+  --optimizer AdagradOptimizer \
+  --lr 0.05 \
+  --batch_norm 1 \
+  --verbose 1 \
+  --early_stop 1 \
+  --epoch 200
