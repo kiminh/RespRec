b7b3261b22751bdcf1c1630c493e42fdaf59cc97
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2019-06-17 18:11:06 +1000

    x

--- UbsRec/nfm/run_all.sh
+++ UbsRec/nfm/run_all.sh
@@ -1,6 +1,6 @@
 python NeuralFM.py \
   --path ~/Downloads/data/ \
-  --dataset ml-tag \
+  --dataset frappe \
   --hidden_factor 64 \
   --layers '[64]' \
   --keep_prob '[0.8,0.5]' \
@@ -14,6 +14,7 @@ python NeuralFM.py \
   --early_stop 1 \
   --epoch 200
 exit
+# --dataset ml-tag
 
 python NeuralFM.py \
   --path ~/Downloads/data/ \
