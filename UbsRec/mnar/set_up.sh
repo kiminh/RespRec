843dec86bd5eadadb0a4a7860f81ce33d37a09d2
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2019-07-02 20:37:52 +1000

    x

--- UbsRec/mnar/set_up.sh
+++ UbsRec/mnar/set_up.sh
@@ -1,15 +1,7 @@
-# python set_new.py book resp
-# python set_new.py movie resp
+python set_up.py book lib
+# python set_up.py movie lib
 exit
 
-python set_old.py book lib
-python set_old.py movie lib
+python set_new.py book resp
+python set_new.py movie resp
 exit
-
-
-
-
-
-
-
-
