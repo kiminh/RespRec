fb176e4e1b9130aeffbc61b80807c5671dbf1e58
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2020-01-10 15:13:04 -0800

    x

--- UbsRec/mar/script/ltr_music.sh
+++ UbsRec/mar/script/ltr_music.sh
@@ -12,11 +12,41 @@ outer_lr=0.005
 keep_probs='[0.8,0.8]'
 layer_sizes='[64]'
 base_model=fm
+meta_model=param
 n_epoch=4
 n_factor=128
 n_trial=4
 opt_type=adagrad
 verbose=0
+
+for var_reg in 0 1; do
+  python -W ignore ../run_ltr.py \
+    --data_dir ${data_dir} \
+    --all_reg ${all_reg} \
+    --batch_norm ${batch_norm} \
+    --batch_size ${batch_size} \
+    --by_batch ${by_batch} \
+    --by_epoch ${by_epoch} \
+    --i_input ${i_input} \
+    --i_disc_input ${i_disc_input} \
+    --i_cont_input ${i_cont_input} \
+    --inner_lr ${inner_lr} \
+    --outer_lr ${outer_lr} \
+    --keep_probs ${keep_probs} \
+    --layer_sizes ${layer_sizes} \
+    --base_model ${base_model} \
+    --meta_model ${meta_model} \
+    --n_epoch ${n_epoch} \
+    --n_factor ${n_factor} \
+    --n_trial ${n_trial} \
+    --opt_type ${opt_type} \
+    --var_reg ${var_reg} \
+    --verbose ${verbose}
+  # exit
+done
+exit
+
+################################################################
 
 by_batch=40
 n_epoch=2
