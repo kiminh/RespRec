2ea4bbf3e175ac8e260261fd57bfae536015408c
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2020-01-14 11:56:27 +1100

    x

--- MulClf/run_once.sh
+++
@@ -1,8 +0,0 @@
-CUDA_VISIBLE_DEVICES=5  nice python ood_BDEK_DomainInfer.py\
-    --num_pretrain_epochs=0 \
-    --num_train_epochs=150 \
-    --learning_rate=1e-4 \
-    --is_anneal_kl_lambda=True \
-    --is_use_unsup_data=False \
-    --dist_type=Dirichlet \
-    --dist_configure=sparsity_sigmoid
